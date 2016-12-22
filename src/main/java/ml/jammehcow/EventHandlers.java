352588d357ec36d76f63be7de5881ae7a669a845
James Upjohn <jspartan250@gmail.com>
2016-12-22 19:35:08 +1300

    Added basic event handlers

---
+++ src/main/java/ml/jammehcow/EventHandlers.java
@@ -0,0 +1,25 @@
+package ml.jammehcow;
+
+import sx.blah.discord.api.events.EventSubscriber;
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
+import sx.blah.discord.handle.impl.events.ReadyEvent;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.MissingPermissionsException;
+import sx.blah.discord.util.RateLimitException;
+
+/**
+ * Author: jammehcow.
+ * Date: 22/12/16.
+ */
+
+public class EventHandlers {
+    @EventSubscriber
+    public void onReadyEvent(ReadyEvent event) {
+        // Do something amazing and bot-ish.
+    }
+
+    @EventSubscriber
+    public void onMessageReceivedEvent(MessageReceivedEvent event) throws RateLimitException, DiscordException, MissingPermissionsException {
+        // Listen for messages and test against commands.
+    }
+}
