2cf087d8c716447ede501681aef3b8d6d1a52d24
James Upjohn <jspartan250@gmail.com>
2016-12-23 18:02:49 +1300

    Finally added logger
    After stupid issues

--- src/main/java/ml/jammehcow/Main.java
+++ src/main/java/ml/jammehcow/Main.java
@@ -1,6 +1,7 @@
 package ml.jammehcow;
 
-import org.yaml.snakeyaml.Yaml;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.util.DiscordException;
@@ -11,14 +12,23 @@ import sx.blah.discord.util.DiscordException;
  */
 
 public class Main {
+
+
+    public static final Logger logger  = LoggerFactory.getLogger(Main.class);
+    public static final double REV     = 1.0;
+
     // Will replace with config values
     private static final String token  = "bottoken";
     private static final String prefix = "!";
 
+    public static void main(String[] args) {
+        logger.info("Starting Hara v" + REV);
+    }
+
     public static IDiscordClient getClient(String token) throws DiscordException {
         ClientBuilder clientBuilder = new ClientBuilder();
         clientBuilder.withToken(token);
-        return clientBuilder.login();
 
+        return clientBuilder.login();
     }
 }
