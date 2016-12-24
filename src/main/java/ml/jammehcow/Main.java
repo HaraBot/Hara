4e34807aa23ec9db5c295d892a9e907cb94bf58b
James Upjohn <jspartan250@gmail.com>
2016-12-24 13:33:54 +1300

    Implemented config loader in getClient()

--- src/main/java/ml/jammehcow/Main.java
+++ src/main/java/ml/jammehcow/Main.java
@@ -6,28 +6,34 @@ import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.util.DiscordException;
 
+import java.util.Map;
+
+import static ml.jammehcow.ConfigWrapper.getConfig;
+
 /**
  * Author: jammehcow.
  * Date: 21/12/16.
  */
 
 public class Main {
-
-
+    // Sets logger to SLF4J with logback
     public static final Logger logger  = LoggerFactory.getLogger(Main.class);
+    // Grab that config!
+    public static Config config        = getConfig();
+
     public static final double REV     = 1.0;
 
     // Will replace with config values
-    private static final String token  = "bottoken";
-    private static final String prefix = "!";
+    private static final String prefix = config.prefix;
+
 
     public static void main(String[] args) {
         logger.info("Starting Hara v" + REV);
     }
 
-    public static IDiscordClient getClient(String token) throws DiscordException {
+    public static IDiscordClient getClient() throws DiscordException {
         ClientBuilder clientBuilder = new ClientBuilder();
-        clientBuilder.withToken(token);
+        clientBuilder.withToken(config.token);
 
         return clientBuilder.login();
     }
