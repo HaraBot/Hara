5e453993fe4738f00634c245c898d197d9801bbf
James Upjohn <jspartan250@gmail.com>
2017-03-25 16:52:28 +1300

    Added config options to config

--- src/main/java/ml/jammehcow/Config/Config.java
+++ src/main/java/ml/jammehcow/Config/Config.java
@@ -9,4 +9,7 @@ public class Config {
     public int rev;
     public String token;
     public String prefix;
+    public boolean sql;
+    public boolean lavaplayer;
+    public String youtubeAPIKey;
 }
