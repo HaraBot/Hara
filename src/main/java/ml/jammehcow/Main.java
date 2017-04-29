72a7a7292317a9fa8c794f2445c00dce3b564355
James Upjohn <jspartan250@gmail.com>
2017-04-30 09:46:08 +1200

    Removed erroneous false event handler register

--- src/main/java/ml/jammehcow/Main.java
+++ src/main/java/ml/jammehcow/Main.java
@@ -2,7 +2,6 @@ package ml.jammehcow;
 
 import ml.jammehcow.Config.Config;
 import ml.jammehcow.Handlers.EventHandlers;
-import ml.jammehcow.Handlers.MessageHandler;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -53,7 +52,6 @@ public class Main {
             client = getClient();
             EventDispatcher dispatcher = client.getDispatcher();
             dispatcher.registerListener(new EventHandlers());
-            dispatcher.registerListener(new MessageHandler());
         }
     }
 
